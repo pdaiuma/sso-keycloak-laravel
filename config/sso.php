8a48ca66e17b0516cd068bb0210a22d4233eb070
adr2810 <andry@uma.ac.id>
2024-10-10 10:26:23 +0700

    Initial commit

---
+++ config/sso.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'base_url' => env('KEYCLOAK_BASE_URL'),
+    'realm' => env('KEYCLOAK_REALM'),
+    'client_id' => env('KEYCLOAK_CLIENT_ID'),
+    'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
+    'callback' => env('KEYCLOAK_CALLBACK'),
+];
